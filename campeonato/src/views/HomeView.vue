28c14e0e75e3acd8f97e860fae7cfd189e37b6f0
Filipa-SRocha <filipa.sr2@gmail.com>
2022-03-18 22:47:39 +0000

    added Home View

--- campeonato/src/views/HomeView.vue
+++ campeonato/src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <template>
 	<div class="home-view">
 		<TopBar />
+		<h1 class="h3 text-center">Brasileirão Série A</h1>
+		<div id="conteudo" class="d-flex flex-column align-center">
+			<v-img class="my-6" src="../assets/brasileirao-imagem.jpeg" />
+			<p class="my-6 text-center px-2">
+				A série A do campeonato Brasileiro de Futebol de 2020, oficialmente
+				Brasileirão Açaí - Série A 2020 por motivos de patrocinio, é a 64º
+				edição da priincipal divisão do futebol Brasileiro. A disputa tem o
+				mesmo regulamento dos anos anteriores, quando foi implementado o sistema
+				de pontos corridos.
+			</p>
+		</div>
 	</div>
 </template>
 
